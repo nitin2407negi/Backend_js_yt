1c0942df6ede98c14e179919b349f36593a739eb
nitin2407negi <nitinnegi241@gmail.com>
2024-06-14 17:10:27 +0530

    express install

--- day2_express/index.js
+++ day2_express/index.js
@@ -1,7 +1,15 @@
 //installing express:- npm i express
+  
+//express=>backend web application framework hai nodejs ka api banane ke liye 
+
+//node module=>ye ek engine hai jsime saare packages install honge jo pure application ko chlate hai
+
+//package.json=>description hai jese bill
+                      
 
 //creating express server
+
 import express from 'express'
 const app=express();
-const port=3000;
-app.listen(port,()=>console.log(`express server is running on ${port}`))
+const port =3000;
+app.listen(port,()=>console.log(`express server is running on ${port}`));
